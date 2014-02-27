86b32baf0daa2599146c1ec1309ab8f4a0e6a8f4
darrentrejo <darren.trejo@gmail.com>
2014-02-27 17:54:13 -0600

    Removed card SVG's, now using canvas

--- js/vpFunctions.js
+++ js/vpFunctions.js
@@ -1,6 +1,5 @@
 var deck;
 var playersHand;
-var svgCache = {};
 
 	function getRandomNumbers()
    	{
@@ -47,40 +46,32 @@ var svgCache = {};
 		}
 	}
 	
-	function loadCard(cardValueSuit, divId) {
-		var cardSlot = document.getElementById(divId);
-		var cardId = "card" + divId.slice(-1);
-		cardSlot.innerHTML = '';
-		if(typeof svgCache[cardValueSuit] != "undefined")
+	function loadCard(cardValue, cardSuit, canvasId) {
+		var cardSlot = document.getElementById(canvasId);
+		var canvas = document.getElementById(canvasId);
+        canvas.width = parseInt(canvas.parentNode.offsetWidth);
+        canvas.height = parseInt(canvas.parentNode.offsetHeight);
+        var context = canvas.getContext("2d");		
+		var suitY = canvas.height / 2 - 5;
+        drawPip(context, cardValue, cardSuit);
+		switch(cardSuit)
 		{
-			var svg = svgCache[cardValueSuit];
-			svg = svg.documentElement;
-			svg = document.importNode(svg, true);
-			svg = sizeAndIdCard(svg, cardId);
-			cardSlot.appendChild(svg);
-		}
-		else
-		{
-			var xhr;
-			if (window.XMLHttpRequest)
-			{
-				xhr=new XMLHttpRequest();
-			}
-			else
-			{
-				xhr=new ActiveXObject("Microsoft.XMLHTTP");
-			}
-			xhr.open('get', 'https://dl.dropboxusercontent.com/u/17091314/PlayingCards/' + cardValueSuit + '.svg', true);
-			xhr.onreadystatechange = function () {
-				if (xhr.readyState != 4 && xhr.status==200) return;
-				var svg = xhr.responseXML;
-				svgCache[cardValueSuit] = svg;
-				svg = svg.documentElement;
-				svg = document.importNode(svg, true);
-				svg = sizeAndIdCard(svg, cardId);
-				cardSlot.appendChild(svg);
-			};
-			xhr.send();
+			case "S":
+				drawSpade(context,  canvas.width * 0.5, suitY, 37.5, 50);
+				drawSpade(context, 11.875, 22, 18.75, 25);
+				break;
+			case "H":
+				drawHeart(context,  canvas.width * 0.5, suitY, 37.5, 50);
+				drawHeart(context, 11.875, 22, 18.75, 25);
+				break;
+			case "C":
+				drawClub(context,  canvas.width * 0.5, suitY, 37.5, 50);
+				drawClub(context, 11.875, 22, 18.75, 25);
+				break;
+			case "D":
+				drawDiamond(context,  canvas.width * 0.5, suitY, 37.5, 50);
+				drawDiamond(context, 11.875, 22, 18.75, 25);
+				break;
 		}
     }
 	
@@ -91,7 +82,9 @@ var svgCache = {};
 		playersHand = new Array();
 		for (var index = 0; index < 5; index++)
 		{
-			loadCard(deck[index], "cardSlot" + (index + 1));
+			var cardValue = deck[index].substring(0,deck[index].length - 1);
+			var cardSuit = deck[index].slice(-1);
+			loadCard(cardValue, cardSuit, "canvas" + (index + 1));
 			document.getElementById("holdStatus" + (index + 1)).innerHTML = "";
 			document.getElementById("holdButton" + (index + 1)).className = "unselected";
 			playersHand[index] = deck[index];
@@ -118,7 +111,9 @@ var svgCache = {};
 				deckIndex();
 				var cardByValueSuit = deck[randomNumbers[randomNumbers.length - 1]];
 				playersHand[index] = cardByValueSuit;
-				loadCard(cardByValueSuit, "cardSlot" + (index + 1));
+				var cardValue = playersHand[index].substring(0,playersHand[index].length - 1);
+				var cardSuit = cardByValueSuit.slice(-1);
+				loadCard(cardValue, cardSuit, "canvas" + (index + 1));
 			}
 		}
 	}
@@ -453,15 +448,6 @@ var svgCache = {};
 		}
 	}
 	
-	function sizeAndIdCard(svg, cardId) {
-		svg.setAttribute("id", cardId);
-		svg.setAttribute("width", "235");
-		svg.setAttribute("height", "333");
-		svg.setAttribute("preserveAspectRatio", "xMinYMin meet");
-		svg.setAttribute("viewBox", "260 370 744.09448819 1052.3622047");
-		return svg;
-	}
-	
 	function toggleSelection(callingButton) {
 		var slotNumber = callingButton.id.slice(-1);
 		if (callingButton.className == 'selected')
@@ -474,3 +460,191 @@ var svgCache = {};
 			callingButton.className = 'selected';
 		}
 	}
+	function drawSpade(context, x, y, width, height) {
+            context.save();
+            var bottomWidth = width * 0.7;
+            var topHeight = height * 0.7;
+            var bottomHeight = height * 0.3;
+
+            context.beginPath();
+            context.moveTo(x, y);
+
+            // top left of spade          
+            context.bezierCurveTo(
+                x, y + topHeight / 2, // control point 1
+                x - width / 2, y + topHeight / 2, // control point 2
+                x - width / 2, y + topHeight // end point
+            );
+
+            // bottom left of spade
+            context.bezierCurveTo(
+                x - width / 2, y + topHeight * 1.3, // control point 1
+                x, y + topHeight * 1.3, // control point 2
+                x, y + topHeight // end point
+            );
+
+            // bottom right of spade
+            context.bezierCurveTo(
+                x, y + topHeight * 1.3, // control point 1
+                x + width / 2, y + topHeight * 1.3, // control point 2
+                x + width / 2, y + topHeight // end point
+            );
+
+            // top right of spade
+            context.bezierCurveTo(
+                x + width / 2, y + topHeight / 2, // control point 1
+                x, y + topHeight / 2, // control point 2
+                x, y // end point
+            );
+
+            context.closePath();
+            context.fill();
+
+            // bottom of spade
+            context.beginPath();
+            context.moveTo(x, y + topHeight);
+            context.quadraticCurveTo(
+                x, y + topHeight + bottomHeight, // control point
+                x - bottomWidth / 2, y + topHeight + bottomHeight // end point
+            );
+            context.lineTo(x + bottomWidth / 2, y + topHeight + bottomHeight);
+            context.quadraticCurveTo(
+                x, y + topHeight + bottomHeight, // control point
+                x, y + topHeight // end point
+            );
+            context.closePath();
+            context.fillStyle = "black";
+            context.fill();
+            context.restore();
+        }
+
+        function drawHeart(context, x, y, width, height) {
+            context.save();
+            context.beginPath();
+            var topCurveHeight = height * 0.3;
+            context.moveTo(x, y + topCurveHeight);
+            // top left curve
+            context.bezierCurveTo(
+                x, y,
+                x - width / 2, y,
+                x - width / 2, y + topCurveHeight
+            );
+
+            // bottom left curve
+            context.bezierCurveTo(
+                x - width / 2, y + (height + topCurveHeight) / 2,
+                x, y + (height + topCurveHeight) / 2,
+                x, y + height
+            );
+
+            // bottom right curve
+            context.bezierCurveTo(
+                x, y + (height + topCurveHeight) / 2,
+                x + width / 2, y + (height + topCurveHeight) / 2,
+                x + width / 2, y + topCurveHeight
+            );
+
+            // top right curve
+            context.bezierCurveTo(
+                x + width / 2, y,
+                x, y,
+                x, y + topCurveHeight
+            );
+
+            context.closePath();
+            context.fillStyle = "red";
+            context.fill();
+            context.restore();
+        }
+
+        function drawClub(context, x, y, width, height) {
+            context.save();
+            var circleRadius = width * 0.3;
+            var bottomWidth = width * 0.5;
+            var bottomHeight = height * 0.35;
+            context.fillStyle = "black";
+
+            // top circle
+            context.beginPath();
+            context.arc(
+                x, y + circleRadius + (height * 0.05),
+                circleRadius, 0, 2 * Math.PI, false
+            );
+            context.fill();
+
+            // bottom right circle
+            context.beginPath();
+            context.arc(
+                x + circleRadius, y + (height * 0.6),
+                circleRadius, 0, 2 * Math.PI, false
+            );
+            context.fill();
+
+            // bottom left circle
+            context.beginPath();
+            context.arc(
+                x - circleRadius, y + (height * 0.6),
+                circleRadius, 0, 2 * Math.PI, false
+            );
+            context.fill();
+
+            // center filler circle
+            context.beginPath();
+            context.arc(
+                x, y + (height * 0.5),
+                circleRadius / 2, 0, 2 * Math.PI, false
+            );
+            context.fill();
+
+            // bottom of club
+            context.moveTo(x, y + (height * 0.6));
+            context.quadraticCurveTo(
+                x, y + height,
+                x - bottomWidth / 2, y + height
+            );
+            context.lineTo(x + bottomWidth / 2, y + height);
+            context.quadraticCurveTo(
+                x, y + height,
+                x, y + (height * 0.6)
+            );
+            context.closePath();
+            context.fill();
+            context.restore();
+        }
+
+        function drawDiamond(context, x, y, width, height) {
+            context.save();
+            context.beginPath();
+            context.moveTo(x, y);
+
+            // top left edge
+            context.lineTo(x - width / 2, y + height / 2);
+
+            // bottom left edge
+            context.lineTo(x, y + height);
+
+            // bottom right edge
+            context.lineTo(x + width / 2, y + height / 2);
+
+            // closing the path automatically creates
+            // the top right edge
+            context.closePath();
+
+            context.fillStyle = "red";
+            context.fill();
+            context.restore();
+        }
+
+        function drawPip(context, value, suit) {
+            context.save();
+            context.font = "bold 20pt serif";
+        	if (suit == "H" || suit == "D")
+        		context.fillStyle = "red";
+            context.fillText(value, 5, 20);
+            context.restore();
+        }
+		
+		function clearCardCanvas(canvasId, width, height, context)
+		{
+			context.clearRect(0, 0, width, height);
+		}
